af9e99a03c0998c42f701d627e5e23a375693b6a
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-06-05 22:11:26 +0200

    Set up routes + dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Home Page</div>;
+  return <div><h1>This is the home page</h1></div>;
 }
---
+++ pages/post/[postId].js
@@ -0,0 +1,3 @@
+export default function Post() {
+    return <div><h1>This is the post page</h1></div>;
+  }

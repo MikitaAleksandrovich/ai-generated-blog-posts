7f482d7d5db86632afadecee2b96d625f80c3afa
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-06-27 22:31:03 +0200

    Pass topic and keywords of post dynamically

--- pages/post/new.js
+++ pages/post/new.js
@@ -4,9 +4,16 @@ import { useState } from "react";
 
 export default function NewPost() {
   const [postContent, setPostContent] = useState("");
-  const handleGeneratePost = async () => {
+  const [topic, setTopic] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     const response = await fetch("/api/generatePost", {
       method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ topic, keywords }),
     });
 
     const json = await response.json();
@@ -16,10 +23,32 @@ export default function NewPost() {
 
   return (
     <div>
-      <h1>This is new post page</h1>
-      <button className="btn" onClick={handleGeneratePost}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={topic}
+            onChange={(event) => setTopic(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={keywords}
+            onChange={(event) => setKeywords(event.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+
       <div
         className="msx-w-screen-sm p-10"
         dangerouslySetInnerHTML={{ __html: postContent }}

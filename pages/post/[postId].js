c50af808f44fb79aed29c410887b8bac25f78532
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-07-10 23:06:48 +0200

    Load the post data for a particular id from url

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,9 +1,10 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { faLowVision, faMoneyBillWaveAlt, faVenusMars } from "@fortawesome/free-solid-svg-icons";
-import { warnOptionHasBeenMovedOutOfExperimental } from "next/dist/server/config";
-import { makeResolver } from "next/dist/server/router";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
+import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
-export default function Post() {
+export default function Post(props) {
+  console.log("props", props);
   return (
     <div>
       <h1>This is the post page</h1>
@@ -11,17 +12,46 @@ export default function Post() {
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(() => {
-  return {
-    props: {},
-  };
+Post.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    // Get user data
+    const userSession = await getSession(ctx.req, ctx.res);
+
+    // Set up mongo db connection to collection
+    const client = await clientPromise;
+    const db = client.db("BlogStandard");
+
+    // Find current user in collection
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+
+    // Find post by id from url
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postId),
+      userId: user._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+      },
+    };
+  },
 });
-removeEventListener;matchMedia
-
-
-fdvkm
-fmvkmmvkrfmrfmmfrmkfsmv;sdmvkvmfsdv
-
-df;vmkldsvmmvfsdklv
-
-sfkdmvmmvmdslvmsdfl

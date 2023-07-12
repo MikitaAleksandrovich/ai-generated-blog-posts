17e0262f82dae138f929efefe8586ef3aa47f404
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-07-12 17:06:57 +0200

    Display list of posts and tokens from db

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -5,10 +5,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { Logo } from "../Logo";
 
-export const AppLayout = ({ children, ...rest }) => {
+export const AppLayout = ({ children, availableTokens, posts }) => {
   const { user } = useUser();
-
-  console.log("app props", rest);
 
   return (
     // Set up 2 columns (first is 300px with, second is remaining space)
@@ -21,11 +19,19 @@ export const AppLayout = ({ children, ...rest }) => {
           </Link>
           <Link href="/token-topup" className="block mt-2 text-center ">
             <FontAwesomeIcon icon={faCoins} className="text-yellow-500" />
-            <span className="pl-1">0 tokens available</span>
+            <span className="pl-1">{availableTokens} tokens available</span>
           </Link>
         </div>
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
-          list of posts
+          {posts.map((post) => (
+            <Link
+              key={post.id}
+              href={`/post/${post._id}`}
+              className="block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm"
+            >
+              {post.topic}
+            </Link>
+          ))}
         </div>
         <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
           {!!user ? (

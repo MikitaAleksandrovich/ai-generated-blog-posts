d3f213165c138db4afd5c9e0019d12c2a184e44d
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-07-13 23:04:14 +0200

    Connect stripe for payments without webhooks

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,8 +1,32 @@
 import clientPromise from "../../lib/mongodb";
 import { getSession } from "@auth0/nextjs-auth0";
+import stripeInit from "stripe";
+
+// Set up stripe
+const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
 
 export const handler = async (req, res) => {
   const { user } = await getSession(req, res);
+
+  // Set up stripe price
+  const lineItems = [
+    {
+      price: process.env.STRIPE_PRODUCT_PRICE_ID,
+      quantity: 1,
+    },
+  ];
+
+  // Set up protocol based on env
+  const protocol =
+    process.env.NODE_ENV === "development" ? "http://" : "https://";
+  const host = req.headers.host;
+
+  // Create stripe checkout session
+  const checkoutSession = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: `${protocol}${host}/success`,
+  });
 
   // Set up mongo db connection to collection
   const client = await clientPromise;
@@ -26,7 +50,7 @@ export const handler = async (req, res) => {
     }
   );
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ session: checkoutSession });
 };
 
 export default handler;

535bbb05e19679b80d5c8fb1ef12419872f857ba
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-07-14 13:55:23 +0200

    Add loading functionallity

--- pages/post/new.js
+++ pages/post/new.js
@@ -3,23 +3,32 @@ import { useRouter } from "next/router";
 import { AppLayout } from "../../components/AppLayout";
 import { useState } from "react";
 import { getAppProps } from "../../utils/getAppProps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
 
 export default function NewPost() {
   const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch("/api/generatePost", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ topic, keywords }),
-    });
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api/generatePost", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ topic, keywords }),
+      });
 
-    const json = await response.json();
+      const json = await response.json();
+    } catch (error) {
+      setIsLoading(false);
+    }
+
     if (json?.postId) {
       router.push(`/post/${json.postId}`);
     }
@@ -27,37 +36,49 @@ export default function NewPost() {
 
   return (
     <div className="h-full overflow-hidden">
-      <div className="w-full h-full flex flex-col overflow-auto ">
-        <form
-          onSubmit={handleSubmit}
-          className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
-        >
+      {!!isLoading && (
+        <div className="text-green-500 flex h-full animate-pulse w-full flex-col justify-center items-center">
+          <FontAwesomeIcon icon={faBrain} className="text-8xl" />
           <div>
-            <label>
-              <strong>Generate a blog post on the topic of:</strong>
-            </label>
-            <textarea
-              className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-              value={topic}
-              onChange={(event) => setTopic(event.target.value)}
-            />
+            <h6>Generating...</h6>
           </div>
-          <div>
-            <label>
-              <strong>Targeting the following keywords:</strong>
-            </label>
-            <textarea
-              className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-              value={keywords}
-              onChange={(event) => setKeywords(event.target.value)}
-            />
-            <small className="block mb-2 ">Separate keywords with a coma</small>
-          </div>
-          <button type="submit" className="btn">
-            Generate
-          </button>
-        </form>
-      </div>
+        </div>
+      )}
+      {!isLoading && (
+        <div className="w-full h-full flex flex-col overflow-auto ">
+          <form
+            onSubmit={handleSubmit}
+            className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+          >
+            <div>
+              <label>
+                <strong>Generate a blog post on the topic of:</strong>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                value={topic}
+                onChange={(event) => setTopic(event.target.value)}
+              />
+            </div>
+            <div>
+              <label>
+                <strong>Targeting the following keywords:</strong>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                value={keywords}
+                onChange={(event) => setKeywords(event.target.value)}
+              />
+              <small className="block mb-2 ">
+                Separate keywords with a coma
+              </small>
+            </div>
+            <button type="submit" className="btn">
+              Generate
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }

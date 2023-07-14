2b3141890ea76ae4e3018e9e15f83463163a3d76
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-07-14 13:38:39 +0200

    Add some styles to links and form

--- pages/post/new.js
+++ pages/post/new.js
@@ -23,37 +23,41 @@ export default function NewPost() {
     if (json?.postId) {
       router.push(`/post/${json.postId}`);
     }
-
-    console.log("json", json);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <strong>Generate a blog post on the topic of:</strong>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            value={topic}
-            onChange={(event) => setTopic(event.target.value)}
-          />
-        </div>
-        <div>
-          <label>
-            <strong>Targeting the following keywords:</strong>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            value={keywords}
-            onChange={(event) => setKeywords(event.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn">
-          Generate
-        </button>
-      </form>
+    <div className="h-full overflow-hidden">
+      <div className="w-full h-full flex flex-col overflow-auto ">
+        <form
+          onSubmit={handleSubmit}
+          className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+        >
+          <div>
+            <label>
+              <strong>Generate a blog post on the topic of:</strong>
+            </label>
+            <textarea
+              className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+              value={topic}
+              onChange={(event) => setTopic(event.target.value)}
+            />
+          </div>
+          <div>
+            <label>
+              <strong>Targeting the following keywords:</strong>
+            </label>
+            <textarea
+              className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+              value={keywords}
+              onChange={(event) => setKeywords(event.target.value)}
+            />
+            <small className="block mb-2 ">Separate keywords with a coma</small>
+          </div>
+          <button type="submit" className="btn">
+            Generate
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
